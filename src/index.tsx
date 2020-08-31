1a6ab1a13ae16b4861976b06cee62a3cd05b8c85
erison <erison.pimentao@gmail,com>
2020-08-31 18:03:48 -0300

    tradução de pagina, adição da logo e estrutura de rotas

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import SignInSide from './SignInSide'
+import SignInSide from './pages/signinside/SignInSide'
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/signinside/SignInSide.tsx
+++ src/pages/signinside/SignInSide.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -9,9 +8,9 @@ import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import sideralpng from '../../../src/assets/sideral-logo.png';
 
 function Copyright() {
   return (
@@ -66,12 +65,9 @@ export default function SignInSide() {
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Acesse SIDERAL SAÚDE
-          </Typography>
+          
+          <img src={sideralpng} height='150' alt='sideralpng' />
+          
           <form className={classes.form} noValidate>
             <TextField
               variant="outlined"
@@ -106,7 +102,7 @@ export default function SignInSide() {
               color="primary"
               className={classes.submit}
             >
-              Sign In
+              acessar
             </Button>
             <Grid container>
               <Grid item xs>
