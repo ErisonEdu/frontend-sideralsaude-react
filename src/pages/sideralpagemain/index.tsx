2b5b87fa6601074d40e6a1f224e9348f2038b2a0
erison <erison.pimentao@gmail,com>
2020-09-02 15:20:27 -0300

    build da aplicação

--- src/pages/sideralpagemain/index.tsx
+++ src/pages/sideralpagemain/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { CardMedia } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 import sideralpng from '../../../src/assets/sideral-logo.png';
+import medicajpg from '../../../src/assets/doctor.jpg';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -25,7 +28,17 @@ const useStyles = makeStyles((theme: Theme) =>
         color: 'black',
     },
     logo: {
-      height: 100,    }
+      height: 100,   
+    },
+    medica: {
+      //height: 700,
+      //width: 900,
+      display: 'flex',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed',
+      backgroundSize: 'contain',
+
+    },
   }),
 );
 
@@ -33,19 +46,26 @@ export default function SideralGroup() {
     const classes = useStyles();
   
     return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <div>
-                <a href="http://localhost:3000"><img src={sideralpng} alt="logo-sideral" className={classes.logo}></img></a>
-            </div>
-            <Typography variant="h6" className={classes.title}>
-              Grupo Sideral
-            </Typography>
-            <Button color="inherit" href="http://localhost:3000/SignInSide" className={classes.login}>Entre</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
+      <>
+        <Box component='span' m={1} p={2}>
+          <div className={classes.root}>
+            <AppBar position="static">
+              <Toolbar>
+                <div>
+                    <a href="http://localhost:3000"><img src={sideralpng} alt="logo-sideral" className={classes.logo}></img></a>
+                </div>
+                <Typography variant="h6" className={classes.title}>
+                  Grupo Sideral
+                </Typography>
+                <Button color="inherit" href="http://localhost:3000/SignInSide" className={classes.login}>Entre</Button>
+              </Toolbar>
+            </AppBar>
+          </div>
+        </Box>
+        <CardMedia>
+            <img src={medicajpg} alt="medica" className={classes.medica}></img>
+        </CardMedia>
+      </>
     );
   }
 
